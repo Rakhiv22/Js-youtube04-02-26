1090984f9e7ee5b54f1bf537cdf7661e64ffb9f1
Rakhiv22 <rakhiv9264@gmail.com>
2026-02-04 17:24:56 +0000

    conversion of datatype

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = true
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1, false => 0
+
+let isLoggedIn = "rakhi"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 =>true; 0 => false
+//"" =>false
+//"rakhi" => true
+
+let someNumber = 33
+let sringNumber = String(someNumber)
+console.log(typeof sringNumber);
+
+
