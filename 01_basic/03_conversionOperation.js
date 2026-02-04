da2dd73948fc87e27f3e61a1644bcd2bf7fa5c48
Rakhiv22 <rakhiv9264@gmail.com>
2026-02-04 18:23:04 +0000

    datatype summary

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -19,6 +19,37 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let sringNumber = String(someNumber)
-console.log(typeof sringNumber);
+//console.log(typeof sringNumber);
+
+
+
+//********************************************Operations*************************/
+let value = 3
+let negValue =-value
+//console.log(negValue);
+
+let str1 = "hello"
+let str2 = "Rakhi"
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log("1"+ 2+2);
+// console.log(1+ 2+"2");
+
+// console.log(+true);
+// console.log(+"");
+let num1,num2,num3
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
 
 
---
+++ 01_basic/05-datatype-summary.js
@@ -0,0 +1,26 @@
+// primitive 
+ // 7 types : string,number,boolearn,null,undefined,symbol,BigInt,
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmmail = null
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+const bigNumber =  4342432324334n
+ //Reference(Nonprimitive)
+ // array,Objects,functions
+
+ const heros = ["shaktiman","naagraj","doga"];
+ let myObj = {
+    name: "Rakhi",
+    age : 22
+ }
+ const myfunction = function(){
+   console.log("hello world");
+   
+ }
+ console.log(typeof anotherId);
+ 
